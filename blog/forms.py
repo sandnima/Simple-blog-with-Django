7a0646cfc30242a448073d314c729a32266de73a
sandnima <46921550+sandnima@users.noreply.github.com>
2021-07-07 00:57:52 +0300

    Categories form created Dir of field set to auto labels bug fixed

--- blog/forms.py
+++ blog/forms.py
@@ -8,6 +8,7 @@ class ArticleModelForm(forms.Form):
         max_length=60,
         widget=forms.TextInput(
             attrs={
+                'dir': 'auto',
                 'class': 'form-control form-control-lg mb-3',
                 'placeholder': 'Title',
                 'aria-label': 'Title',
@@ -19,13 +20,34 @@ class ArticleModelForm(forms.Form):
         max_length=160,
         widget=forms.Textarea(
             attrs={
+                'dir': 'auto',
                 'class': 'form-control',
                 'style': 'height: 100px;',
             },
         ),
     )
-    main_category = forms.CharField(max_length=60)
-    sub_category = forms.CharField(max_length=60)
+    main_category = forms.CharField(
+        max_length=60,
+        widget=forms.TextInput(
+            attrs={
+                'dir': 'auto',
+                'class': 'form-control mb-2',
+                'list': 'main_category_options',
+                'placeholder': 'Main Category',
+            },
+        ),
+    )
+    sub_category = forms.CharField(
+        max_length=60,
+        widget=forms.TextInput(
+            attrs={
+                'dir': 'auto',
+                'class': 'form-control mb-3',
+                'list': 'sub_category_options',
+                'placeholder': 'Sub Category',
+            },
+        ),
+    )
     
         
 class MetaModelForm(forms.ModelForm):
