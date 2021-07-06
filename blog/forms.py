f05a8d1c1905e5917007b92ea234c7a5b7e06625
sandnima <46921550+sandnima@users.noreply.github.com>
2021-07-07 00:04:04 +0300

    Title form created

--- blog/forms.py
+++ blog/forms.py
@@ -4,8 +4,18 @@ from ckeditor.fields import RichTextFormField
 
 
 class ArticleModelForm(forms.Form):
-    title = forms.CharField(max_length=60)
+    title = forms.CharField(
+        max_length=60,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control form-control-lg mb-3',
+                'placeholder': 'Title',
+                'aria-label': 'Title',
+            },
+        ),
+    )
     content = RichTextFormField(max_length=2048)
+    headline = forms.CharField(widget=forms.Textarea, max_length=160)
     main_category = forms.CharField(max_length=60)
     sub_category = forms.CharField(max_length=60)
     
