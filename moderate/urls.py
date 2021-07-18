c080da82e9b3e62643a7412b5338d379c43f5c06
sandnima <46921550+sandnima@users.noreply.github.com>
2021-07-18 14:46:24 +0300

    Article list in moderate app added

--- moderate/urls.py
+++ moderate/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import (
-    moderate
+    moderate,
+    article_list
 )
 
 app_name = 'moderate'
 urlpatterns = [
     path('', moderate, name='index'),
+    path('blog/', article_list, name='article_list_index'),
+    path('blog/p-<int:page>/', article_list, name='article_list'),
 ]
--- moderate/views.py
+++ moderate/views.py
@@ -5,11 +5,29 @@ from django.contrib.auth.decorators import login_required
 from .decorators import user_in_group
 from django.urls import resolve, reverse
 
+from blog.models import Article
+
 
 @login_required
 @user_in_group(group="ContentModerators")
 def moderate(request):
     template_name = 'moderate/moderate.html'
     context = {
+    }
+    return render(request, template_name, context)
+
+
+@login_required
+@user_in_group(group="ContentModerators")
+def article_list(request, page=1):
+    template_name = 'moderate/blog/article_list.html'
+    # user_profile = Profile.objects.get(user=request.user)
+    articles = Article.objects.all().exclude(status='DRF').order_by('-updated_at')
+    paginate_by = 10
+    pages = Paginator(articles, paginate_by)
+    queryset = pages.page(page)
+    context = {
+        'object_list': queryset,
+        'total_pages': pages.num_pages
     }
     return render(request, template_name, context)
