01dacc3809a28dcd70dec6d0697296da02ee0805
sandnima <46921550+sandnima@users.noreply.github.com>
2021-07-25 22:16:35 +0300

    CKEditor5 imported locally

--- webpack/src/js/ckeditor.js
+++ webpack/src/js/ckeditor.js
@@ -1,1 +1,17 @@
-import '@ckeditor/ckeditor5-build-classic';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+ClassicEditor
+    .create( document.querySelector( '#editor' ), {
+        toolbar: {
+            items: [
+                'heading', '|',
+                'bold', 'italic', 'link', 'undo', 'redo', 'bulletedList', 'numberedList', 'blockQuote', '|',
+                'alignment',
+                'code', 'codeBlock', '|',
+            ],
+            shouldNotGroupWhenFull: true
+        },
+    } )
+    .catch( error => {
+        console.error( error );
+    } );
